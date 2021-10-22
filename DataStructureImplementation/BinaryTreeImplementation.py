1fc77946b1becf318e5749d35f85e099439e4978
Vaibhav Shrivastava <innomightmail@gmail.com>
2021-10-22 19:46:35 +0530

    Added basic data structures implementations in python hacktoberfest

---
+++ DataStructureImplementation/BinaryTreeImplementation.py
@@ -0,0 +1,50 @@
+def BinaryTree(r):
+    return [r, [], []]
+
+
+def insertLeft(root, newBranch):
+    t = root.pop(1)
+
+    if len(t) > 1:
+        root.insert(1, [newBranch, t, []])
+
+    else:
+        root.insert(1, [newBranch, [], []])
+
+    return root
+
+
+def insertRight(root, newBranch):
+    t = root.pop(1)
+
+    if len(t) > 1:
+        root.insert(2, [newBranch, [], t])
+
+    else:
+        root.insert(2, [newBranch, [], []])
+
+    return root
+
+
+def get_root_val(root):
+    return root[0]
+
+
+def set_root_val(root, new_val):
+    root[0] = new_val
+
+
+def get_left_child(root):
+    return root[1]
+
+
+def get_right_child(root):
+    return root[2]
+
+
+r = BinaryTree(4)
+insertLeft(r, 3)
+insertLeft(r, 6)
+insertRight(r, 5)
+
+print(r)
